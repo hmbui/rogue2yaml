b97ab77bb9c587716a018f7396b465a8c25d87a9
Hai Bui <hbui@slac.stanford.edu>
2018-05-30 15:42:43 -0700

    ENH: Make env variable names specific to the converter, and update the setup script.

--- setup_rogue2yaml.sh
+++ setup_rogue2yaml.sh
@@ -2,9 +2,9 @@
 
 echo Setting up environment variables...
 
-export ROGUE_2_8_3=/afs/slac.stanford.edu/g/lcls/vol9/package/pyrogue/rogue/v2.8.3/python/
-export AC_PYTHON=/u/re/hbui/local/dev/amc-carrier-core/python/
-export CRYO_DET_SURF_PYTHON=/u/re/hbui/local/dev/cryo-det/firmware/submodules/surf/python/
-export CRYO_DET_DSP_CORE_LIB=/u/re/hbui/local/dev/cryo-det/firmware/common/DspCoreLib/CryoDetXray/python/
+export ROGUE2YAML_ROGUE_2_8_3=/afs/slac.stanford.edu/g/lcls/vol9/package/pyrogue/rogue/v2.8.3/python/
+export ROGUE2YAML_AMC_PYTHON=/u/re/hbui/local/dev/amc-carrier-core/python/
+export ROGUE2YAML_CRYO_DET_SURF_PYTHON=/u/re/hbui/local/dev/cryo-det/firmware/submodules/surf/python/
+export ROGUE2YAML_CRYO_DET_DSP_CORE_LIB=/u/re/hbui/local/dev/cryo-det/firmware/common/DspCoreLib/CryoDetXray/python/
 
 echo Done!
