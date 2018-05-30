46f44446d346b851e195bed2d57c1fbaa4c84399
Hai Bui <hbui@slac.stanford.edu>
2018-05-30 12:34:47 -0700

    Simplify sys.path requirements and also output results.

---
+++ setup_rogue2yaml.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo Setting up environment variables...
+
+export ROGUE_2_8_3=/afs/slac.stanford.edu/g/lcls/vol9/package/pyrogue/rogue/v2.8.3/python/
+export AC_PYTHON=/u/re/hbui/local/dev/amc-carrier-core/python/
+export CRYO_DET_SURF_PYTHON=/u/re/hbui/local/dev/cryo-det/firmware/submodules/surf/python/
+export CRYO_DET_DSP_CORE_LIB=/u/re/hbui/local/dev/cryo-det/firmware/common/DspCoreLib/CryoDetXray/python/
+
+echo Done!
